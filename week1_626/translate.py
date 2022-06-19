5c359a3f228bdfdc49ff3c8e036b281988111334
Seonghun Hong <h2o0318@naver.com>
2022-06-19 17:10:03 +0900

    upload fixed translate

---
+++ week1_626/translate.py
@@ -0,0 +1,21 @@
+translate = {
+    "cane": "dog", 
+    "cavallo": "horse", 
+    "elefante": "elephant", 
+    "farfalla": "butterfly", 
+    "gallina": "chicken", 
+    "gatto": "cat", 
+    "mucca": "cow", 
+    "pecora": "sheep",
+    "ragno" : "spider", 
+    "scoiattolo": "squirrel", 
+    "dog": "cane", 
+    "cavallo": "horse", 
+    "elephant" : "elefante", 
+    "butterfly": "farfalla", 
+    "chicken": "gallina", 
+    "cat": "gatto", 
+    "cow": "mucca", 
+    "spider": "ragno", 
+    "squirrel": "scoiattolo"
+}
