c99c01f65a09a76097e35bdfa323a46965167a06
Seonghun Hong <h2o0318@naver.com>
2022-06-19 16:58:43 +0900

    class number upload

---
+++ week1_626/class.py
@@ -0,0 +1,12 @@
+CLASS_NUMBER = {
+    "butterfly": 0,
+    "cat": 1,
+    "chicken": 2,
+    "cow": 3,
+    "dog": 4,
+    "elephant": 5,
+    "horse": 6,
+    "sheep": 7,
+    "spider": 8,
+    "squirrel": 9
+}
