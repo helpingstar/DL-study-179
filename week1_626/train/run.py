699fd3c720dfa07f9e16b46401ca5a2a1d369087
hsh-dev <h2o0318@snu.ac.kr>
2022-06-26 03:00:15 +0900

    drop last update

--- week1_626/train/run.py
+++ week1_626/train/run.py
@@ -13,12 +13,12 @@ from train.manager import Manager
 
 CFG = {
     "save_path" : "./saved",
-    "model_name" : "resnet",
+    "model_name" : "seresnet",
     
     "train_image_path" : "../train_img",
     "test_image_path" : "../test_img",
 
-    "train_batch_size" : 32,
+    "train_batch_size" : 2,
     "test_batch_size" : 1,
     
     "learning_rate" : 1e-4,
@@ -42,21 +42,30 @@ if __name__ == "__main__":
     # Device Initialize
     device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
     
+    
     # Dataset Initialize
     dataloader = {}
     train_dataset = CustomDataset(CFG["train_image_path"], "train")
-    dataloader["train"] = DataLoader(train_dataset, batch_size = CFG["train_batch_size"], shuffle=True, num_workers=0)
+    dataloader["train"] = DataLoader(train_dataset, batch_size = CFG["train_batch_size"], shuffle=True, num_workers=0, drop_last=True)
     
     valid_dataset = CustomDataset(CFG["train_image_path"], "valid")
-    dataloader["valid"] = DataLoader(valid_dataset, batch_size = CFG["train_batch_size"], shuffle=False, num_workers=0)
+    dataloader["valid"] = DataLoader(valid_dataset, batch_size = CFG["train_batch_size"], shuffle=False, num_workers=0, drop_last=True)
     
     test_dataset = CustomDataset(CFG["test_image_path"], "test")
-    dataloader["test"] = DataLoader(test_dataset, batch_size = CFG["test_batch_size"], shuffle=False, num_workers=0)
+    dataloader["test"] = DataLoader(test_dataset, batch_size = CFG["test_batch_size"], shuffle=False, num_workers=0,  drop_last=False)
 
     # Model Import
-    model = ResNet50()
-    # model = SEResNet50()
+    # model = ResNet50()
+    model = SEResNet50()
     # model = CNN()
+
+    ## MULTI GPU    
+    # NGPU = torch.cuda.device_count()
+    # if NGPU > 1:
+    #     model = torch.nn.DataParallel(model, device_ids=list(range(NGPU)))
+    #     torch.multiprocessing.set_start_method('spawn')
+
+    
     model.to(device)
     
     torchsummary.summary(model, (3, 256, 256), batch_size = 16)
